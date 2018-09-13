400eee842c067228dea29a9c8617dba12e1ba928
Nakayama <akiraichi5430@gmail.com>
2018-09-13 16:47:56 +0900

    ユーザー情報編集機能の追加、およびサインアウト/サインイン機能の追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,18 @@
 class UsersController < ApplicationController
+
+  def edit
+  end
+
+  def update
+    if current_user.update(user_params) #updateできて保存てきた場合
+      redirect_to root_path #root_pathへリダイレクトされる
+    else
+      render :edit  #（失敗した場合）eidtアクションを実行する（再描画する）
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+
 end
