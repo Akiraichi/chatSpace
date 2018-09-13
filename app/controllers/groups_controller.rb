805cf7c9c3c1ef16617daa7ad1bd1b9ed9c4e37a
Nakayama <akiraichi5430@gmail.com>
2018-09-13 20:01:18 +0900

    グループの作成機能およびグループをサイドバーに表示するための機能を実装

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,14 +1,19 @@
 class GroupsController < ApplicationController
+  #グループの新規作成画面
+  def index
+  end
   def new
     @group = Group.new
-    @group.users << current_user
+    @group.users << current_user #現在ログイン中のユーザーを、新規作成したグループに追加
   end
+  
+  #グループの新規作成
   def create
     @group = Group.new(group_params)
     if @group.save
       redirect_to root_path, notice: 'グループを作成しました'
     else
-      render :new
+      render :new #renderはHTTPリクエストを送らず、該当するビューだけを表示
     end
   end
   def edit
