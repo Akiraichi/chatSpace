0a2a5e070d25a44dfb91ede26fc27a0c806aad63
Nakayama <akiraichi5430@gmail.com>
2018-09-13 17:10:18 +0900

    バグフィックス user.scssのインポート処理をapplication.scssに追加 usercontollerにendがなかったため追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,11 +8,13 @@ class UsersController < ApplicationController
       redirect_to root_path #root_pathへリダイレクトされる
     else
       render :edit  #（失敗した場合）eidtアクションを実行する（再描画する）
+    end
   end
 
   private
 
   def user_params
     params.require(:user).permit(:name, :email)
+  end
 
 end
