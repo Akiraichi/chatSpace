0019542c8fb96266b7a52368aad654f19263477e
Nakayama <akiraichi5430@gmail.com>
2018-09-13 17:46:24 +0900

    index/hamlを表示するため一時的にroutesを変更 aタグをlink_toタグに修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :authenticate_user!
-  before_action :configure_permitted_parameters, if: :devise_controller? #deviseにまつわる画面に行った時にということ
+  #before_action :authenticate_user!
+  #before_action :configure_permitted_parameters, if: :devise_controller? #deviseにまつわる画面に行った時にということ
 
   protected#クラス内、同一パッケージ、サブクラスからアクセス可
 
