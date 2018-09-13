57325906014abc0544105ab66649bce5f34390cf
Nakayama <akiraichi5430@gmail.com>
2018-09-13 20:53:35 +0900

    updateアクションを追加し、サイドバーのグループを動的に変更するよう処理を修正

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,6 @@
 class GroupsController < ApplicationController
+  before_action :set_group, only: [:edit, :update]
+
   #グループの新規作成画面
   def index
   end
@@ -16,14 +18,23 @@ class GroupsController < ApplicationController
       render :new #renderはHTTPリクエストを送らず、該当するビューだけを表示
     end
   end
-  def edit
-  end
+
+  #editアクションがないが、ルーティングとルーティングに対応するビューさえあれば、明示的にコントローラのアクションを定義しなくても、ビューを表示することができるため、問題ない。
+
   def update
+    if @group.update(group_params)#更新の可否にあわせて処理する
+      redirect_to group_messages_path(@group), notice: 'グループを編集しました'
+    else
+      render :edit
+    end
   end
 
 
   private
   def group_params
     params.require(:group).permit(:name, { :user_ids => [] })
+  end
+  def set_group
+    @group = Group.find(params[:id])
   end
 end
