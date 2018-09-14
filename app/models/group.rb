60fe3524fb8deec7ef0ddc06b6458c3e57f33ae4
Nakayama <akiraichi5430@gmail.com>
2018-09-14 14:47:32 +0900

    メッセージ表示機能を実装した。groupモデルとchatSide__bodyのhamlを編集した

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,12 @@ class Group < ApplicationRecord
   has_many :users, through: :group_users
   has_many :messages
   validates :name, presence: true
+
+  def show_last_message #メッセージが投稿されている場合、されていない場合で処理を分ける
+    if (last_message = messages.last).present? #最新のメッセージを変数last_messageに代入
+      last_message.content? ? last_message.content : '画像が投稿されています'
+    else
+      'まだメッセージはありません。'
+    end
+  end
 end
