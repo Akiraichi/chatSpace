b43c5a2f50aa62e8b1e48ab028a286ce21b84f0b
Nakayama <akiraichi5430@gmail.com>
2018-09-16 21:09:34 +0900

    非同期通信でコメントが保存されるようにした

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,16 @@
 $(function () {
   $('.chatMain__footer--newMessage').submit(function (e) {
     e.preventDefault();
-    console.log(this)
-  })
-})
+    var formData = new FormData(this); //フォームに入力された値をまとめて取得
+    var url = $(this).attr('action'); //action属性にリンク先のURLがあるため
+    console.log(url);
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+  });
+});
