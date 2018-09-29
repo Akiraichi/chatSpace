e482bd2522236fefb1b8bb000e6c537796bc833a
Nakayama <akiraichi5430@gmail.com>
2018-09-29 18:28:19 +0900

    cssのつけ忘れがあったので修正 submit処理のDOMのid名を変更

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -21,7 +21,7 @@ $(function () {
     return html;
   };
 
-  $('#chatMain__footer--newMessage').submit(function (e) {
+  $('#newMessage').submit(function (e) {
     e.preventDefault();
     var formData = new FormData(this); //フォームに入力された値をまとめて取得
     var url = $(this).attr('action'); //action属性にリンク先のURLがあるため
