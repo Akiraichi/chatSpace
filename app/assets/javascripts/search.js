4ed969cf9830d84d4d68d6b9c22e207d56b0e1e8
Nakayama <akiraichi5430@gmail.com>
2018-09-29 10:51:31 +0900

    追加ボタンを押すとその要素が消える処理を追加

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -6,16 +6,24 @@ $(function () {
                   <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
                 </div>`;
     member_list.append(html);
-}
-
-  $(document).click("user-search-add chat-group-user__btn chat-group-user__btn--add", function () {
+  };
+  function appendChatMenber(user) {
+    var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
+                  <input name='group[user_ids][]' type='hidden' value='${user.id}'>
+                  <p class='chat-group-user__name'>${user.name}</p>
+                  <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+                </div>`;
+    $("#chat-group-users").append(html);
+  };
+  
+  
+  $("#user-search-result").on("click", ".chat-group-user", function () {
     $(this).remove();
-
-  })
+    debugger;
+  });
   
   $(".chat-group-form__input").on("keyup", function () {
     var input = $("#user-search-field").val();
-    debugger;
     $.ajax({
       type: 'GET',
       url: '/users',
@@ -26,7 +34,6 @@ $(function () {
     })
       .done(function (users) {
         member_list.empty();
-        debugger;
         if (users.length !== 0) {
           users.forEach(function (user) {
             appendMenber(user);
